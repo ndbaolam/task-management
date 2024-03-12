8ac402e4c4fe04501d9c2571bde13a9f0095e73c
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-12 18:03:10 +0700

    Xoa, cors, dang ky tk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const database = require('./config/database');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const routesApiVer1 = require('./api/v1/routers/index.route');
@@ -9,6 +10,15 @@ database.connect();
 
 const app = express();
 const port = process.env.PORT;
+
+// var corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
